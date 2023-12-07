6bc27310187705409465a1ffd080b5b361a7dbe0
Cent <147336831+hikusama@users.noreply.github.com>
2023-12-08 01:21:10 +0800

    Add files via upload

---
+++ AmbatuNew/s.js
@@ -0,0 +1,7 @@
+var overtap = document.querySelector('.overtop');
+
+setTimeout(function () {
+    var loader = document.getElementById('loader');
+    loader.classList.add('hidden');
+    overtap.classList.add('oberhid');
+  }, 4000);
