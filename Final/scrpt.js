751949e05c051d36ebc7580442f7383ec4dd3e2f
Hiku Desktop <leomessi25k@gmail.com>
2023-12-12 00:35:06 +0800

    newly

---
+++ Final/scrpt.js
@@ -0,0 +1,8 @@
+// function scrollToElement(elementSelector, instance=0){
+
+//   const elements = document.querySelectorAll(elementSelector);
+//   if (elements.lenght > instance){
+//     elements[instance]ScrollToView
+//   }
+// }
+
